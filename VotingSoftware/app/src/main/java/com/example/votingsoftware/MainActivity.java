4b41aa7dcf9d572c171574fc51e306d81ec68f08
gmeisinger <gmeisinger@gmail.com>
2020-02-18 01:03:39 -0500

    connects to SIS server

--- VotingSoftware/app/src/main/java/com/example/votingsoftware/MainActivity.java
+++ VotingSoftware/app/src/main/java/com/example/votingsoftware/MainActivity.java
@@ -3,8 +3,29 @@ package com.example.votingsoftware;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "VotingSoftware";
+
+    private static Button connectToServerButton,registerToServerButton;
+
+    private EditText serverIp,serverPort;
+
+    static ComponentSocket client;
+
+    private static TextView messageReceivedListText;
 
     private static final String SENDER = "VotingSoftware";
     private static final String REGISTERED = "Registered";
@@ -17,9 +38,116 @@ public class MainActivity extends AppCompatActivity {
     public static final int DISCONNECTED = 2;
     public static final int MESSAGE_RECEIVED = 3;
 
+    static Handler callbacks = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            String str;
+            String[] strs;
+            switch (msg.what) {
+                case CONNECTED:
+                    registerToServerButton.setText(REGISTERED);
+                    Log.e(TAG, "===============================================================CONNECTED" );
+                    break;
+                case DISCONNECTED:
+                    connectToServerButton.setText("Connect");
+                    Log.e(TAG, "===============================================================DISCONNECTED" );
+                    break;
+                case MESSAGE_RECEIVED:
+                    str = (String)msg.obj;
+                    messageReceivedListText.append(str+"********************\n");
+                    final int scrollAmount = messageReceivedListText.getLayout().getLineTop(messageReceivedListText.getLineCount()) - messageReceivedListText.getHeight();
+                    if (scrollAmount > 0)
+                        messageReceivedListText.scrollTo(0, scrollAmount);
+                    else
+                        messageReceivedListText.scrollTo(0, 0);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connectToServerButton = (Button) findViewById(R.id.connectButton);
+        registerToServerButton = (Button) findViewById(R.id.registerButton);
+        serverIp = (EditText) findViewById(R.id.serverIp);
+        serverPort = (EditText) findViewById(R.id.serverPort);
+        messageReceivedListText = (TextView) findViewById(R.id.messageReceivedList);
+        messageReceivedListText.setMovementMethod(ScrollingMovementMethod.getInstance());
+
+        registerToServerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(client!=null && client.isSocketAlive() && registerToServerButton.getText().toString().equalsIgnoreCase(REGISTERED)){
+                    Toast.makeText(MainActivity.this,"Already registered.",Toast.LENGTH_SHORT).show();
+                }else{
+                    client = new ComponentSocket(serverIp.getText().toString(), Integer.parseInt(serverPort.getText().toString()),callbacks);
+                    client.start();
+                    Timer timer = new Timer();
+                    timer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            KeyValueList list = generateRegisterMessage();
+                            client.setMessage(list);
+                        }
+                    }, 500);
+                }
+            }
+        });
+        connectToServerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(connectToServerButton.getText().toString().equalsIgnoreCase(DISCOONECTED)){
+                    Log.e(MainActivity.TAG, "Sending connectToServerButton.2" );
+                    client.killThread();
+                    connectToServerButton.setText("Connect");
+                }else{
+
+                    Timer timer = new Timer();
+                    timer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            KeyValueList list = generateConnectMessage();
+                            client.setMessage(list);
+                        }
+                    }, 100);
+
+                    connectToServerButton.setText(DISCOONECTED);
+                }
+            }
+        });
+    }
+
+    //Generate a test register message, please replace something of attributes with your own.
+    KeyValueList generateRegisterMessage(){
+        KeyValueList list = new KeyValueList();
+        //Set the scope of the message
+        list.putPair("Scope",SCOPE);
+        //Set the message type
+        list.putPair("MessageType","Register");
+        //Set the sender or name of the message
+        list.putPair("Sender",SENDER);
+        //Set the role of the message
+        list.putPair("Role","Basic");
+        return list;
+    }
+    //Generate a test connect message, please replace something of attributes with your own.
+    KeyValueList generateConnectMessage(){
+        KeyValueList list = new KeyValueList();
+        //Set the scope of the message
+        list.putPair("Scope",SCOPE);
+        //Set the message type
+        list.putPair("MessageType","Connect");
+        //Set the sender or name of the message
+        list.putPair("Sender",SENDER);
+        //Set the role of the message
+        list.putPair("Role","Basic");
+        //Set the name of the component
+        list.putPair("Name",SENDER);
+        return list;
     }
 }
